d73ba4ccbbb3e407b84ee2f53635de60123b7a46
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-10-20 23:05:38 +0530

    Changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a client/.next client/package.json cient/package-lock.json travis@68.183.94.49:/home/nxtdoordeals/site
+  cd client
+  rsync -a * travis@68.183.94.49:/home/nxtdoordeals/site
 else
   echo "Not deploying, since the branch isn't main."
 fi
