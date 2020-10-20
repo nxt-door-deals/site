9cf053da8dd65770c239382ebdbf8a08ec1ce361
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-10-20 23:45:05 +0530

    Added public folder in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   ssh-add ~/.ssh/id_rsa
 
   cd client
-  rsync -a .next package.json package-lock.json travis@68.183.94.49:/home/nxtdoordeals/site
+  rsync -a .next public package.json package-lock.json travis@68.183.94.49:/home/nxtdoordeals/site
 else
   echo "Not deploying, since the branch isn't main."
 fi
