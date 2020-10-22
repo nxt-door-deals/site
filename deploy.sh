7fafe85a3d014b03015b348f61c59509b68d3b68
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-10-22 21:42:07 +0530

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -xe
 if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
-
+  
   cd client
-  rsync -a --exclude={'/node_modules'} travis@68.183.94.49:/home/nxtdoordeals/site
+  rsync -a --exclude={'/node_modules', '.gitignore'} * travis@68.183.94.49:/home/nxtdoordeals/site
   echo "Deployed successfully!"
 else
   echo "Not deploying, since the branch isn't main."
