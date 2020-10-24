7ca4c231f115c2eece9ebcc07870017c290946d6
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-10-24 10:24:34 +0530

    Updated keys.js

--- client/utils/keys.js
+++ client/utils/keys.js
@@ -3,11 +3,13 @@ const env = process.env.NEXT_PUBLIC_ENV || "development";
 const keys = {
     development: {
         API_PROXY: "http://localhost:8000/api/v1",
-        FROM_EMAIL: "contact@nxtdoordeals.com"
+        FROM_EMAIL: "contact@nxtdoordeals.com",
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6Ld-xtoZAAAAAB2FrlEY9pGNl69GlPEW_0YKYitH"
     },
     production: {
         API_PROXY: "https://api.nxtdoordeals.com/api/v1",
-        FROM_EMAIL: "contact@nxtdoordeals.com"
+        FROM_EMAIL: "contact@nxtdoordeals.com",
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6Ld-xtoZAAAAAB2FrlEY9pGNl69GlPEW_0YKYitH"
     }
 }[env];
 
