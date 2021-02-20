bfbd5f4a1dca2ffa95dfbb048dcc260b69183b23
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-02-20 16:04:55 +0530

    Redeploying after change in nginx config

--- client/utils/keys.js
+++ client/utils/keys.js
@@ -12,7 +12,7 @@ const keys = {
       "https://nxt-door-deals-test.s3.ap-south-1.amazonaws.com/site-images/default.png",
   },
   production: {
-    API_PROXY: "https://api.nxtdoordeals.com/api/v1",
+    API_PROXY: "http://api.nxtdoordeals.com/api/v1",
     WS_PROXY: "ws://api.nxtdoordeals.com/api/v1",
     FROM_EMAIL: "contact@nxtdoordeals.com",
     SERVER: "68.183.94.49",
