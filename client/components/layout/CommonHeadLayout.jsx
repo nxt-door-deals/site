1ee1df99979f87f7691e5714259edde53aaacae6
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-03-30 13:54:22 +0530

    Updated the favicons

--- client/components/layout/CommonHeadLayout.jsx
+++ client/components/layout/CommonHeadLayout.jsx
@@ -10,7 +10,7 @@ const CommonHeadLayout = () => {
         content="width=device-width, initial-scale=1,maximum-scale=5, minimum-scale=1, viewport-fit=cover"
       />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-      {/* <link rel="shortcut icon" sizes="16x16" href="/favicon.ico" type="image/x-icon" /> */}
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -29,6 +29,10 @@ const CommonHeadLayout = () => {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
+      <meta name="theme-color" content="#ffffff" />
+
       <meta property="og:image" content={keys.ICON} />
       <meta property="og:image:width" content="256" />
       <meta property="og:image:height" content="256" />
