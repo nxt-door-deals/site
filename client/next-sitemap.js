36dd56b596631a1fbaf97821c4ad955abdf1ca1b
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-11-08 22:55:06 +0530

    Updated sitemap config

--- client/next-sitemap.js
+++ client/next-sitemap.js
@@ -1,3 +1,20 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SERVER || "https://nxtdoordeals.com",
+  generateRobotsTxt: true,
+  exclude: ["/404", "/verifyemail", "/alreadyloggedin", "/goodbye", "/logout"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: [
+          "/404",
+          "/verifyemail",
+          "/alreadyloggedin",
+          "/goodbye",
+          "/logout",
+        ],
+      },
+    ],
+    additionalSitemaps: [process.env.NEXT_PUBLIC_SERVER + "/sitemap.xml"],
+  },
 };
