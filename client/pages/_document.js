51c7ea1319c7f94470e9c9e204fbfa4742452f6e
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-11-16 22:41:39 +0530

    Fix for large fa icons issue

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,5 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +13,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <Script>{dom.css()}</Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
